11bf795e183cc53f6dd9451023c8dac3bc1b4593
Brian Zauner <binrz00@hotmail.com>
2020-01-23 11:54:51 -0600

    mouse movement
    mouse movement

--- src/components/paddle.js
+++ src/components/paddle.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./paddle.css";
-function Paddle() {
-  return <div className="paddle" />;
+export default function Paddle() {
+  let [paddleX, setPaddleX] = useState(0);
+  if (paddleX <= 32) {
+    paddleX = 32;
+  }
+  if (paddleX >= 235) {
+    paddleX = 235;
+  }
+
+  function handleMouse(e) {
+    setPaddleX(e.x);
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  console.log(paddleX);
+
+  return <div className="paddle" style={{ left: `${paddleX}px` }} />;
 }
-export default Paddle;
